c6dd8857c48de88e66e0f8510fdf9857a57bed7b
Raymond Julin <raymond.julin@gmail.com>
2010-05-21 19:37:13 +0200

    Added smarty

---
+++ View.php
@@ -0,0 +1,24 @@
+<?php
+require_once('livs/smarty/libs/Smarty.class.php');
+
+/**
+ * Facade over the main Smarty class so to
+ * set some common variables and setup data
+ *
+ * @author Raymond Julin (raymond.julin@keyteq.no)
+ */
+
+class View extends Smarty
+{
+    /**
+     * Constructor
+     *
+     * @return KQSmarty
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->template_dir =  'views/';
+        $this->compile_dir =  'views_c/';
+    }
+}
