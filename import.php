dd0fbc78df9f61622afb2eee97689316d325dcda
Raymond Julin <raymond.julin@gmail.com>
2010-05-21 21:32:46 +0200

    Improved import

--- import.php
+++ import.php
@@ -7,19 +7,52 @@ $db = Config::getDb();
 $file = file('data/23.txt');
 $routeNumber = array_shift($file);
 list($name, $number) = explode(";", $routeNumber);
+
+// Flush routes
+$db->routes->drop();
+$db->routes->ensureIndex(array('id'=>1), array('unique'=>true));
+$db->routes->ensureIndex(array('name'=>1));
+
+$db->stops->drop();
+$db->stops->ensureIndex(array('name'=>1),array('unique'=>true, 'dropDups'=>true));
+
+
 $route = array(
     'id' => $number,
     'name' => $name,
     'stops' => array()
 );
 
+$started = false;
 foreach ($file as $line) {
     $arr = explode(";", $line);
     $stopName = array_shift($arr);
-    $route['stops'][] = array(
-        'name' => $stopName,
-        'times' => $arr
-    );
+    $times = array();
+    foreach ($arr as $s) {
+        $s = str_replace("\n", "", $s);
+        if (preg_match("/[0-9]{1,2}\.[0-9]{1,2}/", $s)) {
+            $started = true;
+            $times[] = $s;
+        }
+    }
+    $db->stops->insert(array('name'=>$stopName));
+    if (count($times) > 0 || $started) {
+        $stop = array(
+            'name' => $stopName,
+            'times' => $times
+        );
+        $route['stops'][] = $stop;
+    }
 }
+// Flip array and remove all entries of stops that have no times
+$flipped = array_reverse($route['stops']);
+
+foreach ($flipped as $key => $tmp) {
+    if (count($tmp['times']) == 0)
+        unset($flipped[$key]);
+    else
+        break;
+}
+$route['stops'] = array_reverse($flipped);
 
 $db->routes->insert($route);
