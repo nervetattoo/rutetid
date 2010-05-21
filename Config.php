f05a22106409bca622c0315e0e6a459cddf9457b
Raymond Julin <raymond.julin@gmail.com>
2010-05-21 20:11:43 +0200

    Cutify

--- Config.php
+++ Config.php
@@ -1,12 +1,23 @@
 <?php
+if (file_exists('my.config.php'))
+    require_once('my.config.php');
+if (!defined('PREFIX'))
+    define('PREFIX', 'main');
 
 class Config {
     private static $path = null;
+    private static $db = null;
     public static function getPath() {
         if (self::$path == null) {
             $path = split("/",pathinfo(__FILE__, PATHINFO_DIRNAME));
             self::$path = join("/", $path) . "/";
         }
         return self::$path;
+    }
+
+    public static function getDb() {
+        $dbName = PREFIX;
+        $mongo = new Mongo;
+        return $mongo->$dbName;
     }
 }
--- index.php
+++ index.php
@@ -2,13 +2,7 @@
 require_once("Config.php");
 require_once("View.php");
 
-if (!defined('PREFIX'))
-    define('PREFIX', 'main');
-$dbName = PREFIX;
-
-// Set up mongo
-$mongo = new Mongo;
-$db = $mongo->$dbName;
+$db = Config::getDb();
 
 $view = new View;
 $view->display('index.tpl');
