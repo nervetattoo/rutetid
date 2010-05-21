c7cb82f0a56b6fc7827170e551b1e9e19e681eaa
Raymond Julin <raymond.julin@gmail.com>
2010-05-21 21:52:45 +0200

    Added support for full import in import script, added route 24

--- import.php
+++ import.php
@@ -4,9 +4,6 @@ require_once("View.php");
 
 $db = Config::getDb();
 
-$file = file('data/23.txt');
-$routeNumber = array_shift($file);
-list($name, $number) = explode(";", $routeNumber);
 
 // Flush routes
 $db->routes->drop();
@@ -16,43 +13,54 @@ $db->routes->ensureIndex(array('name'=>1));
 $db->stops->drop();
 $db->stops->ensureIndex(array('name'=>1),array('unique'=>true, 'dropDups'=>true));
 
+$dir = scandir('data/');
 
-$route = array(
-    'id' => $number,
-    'name' => $name,
-    'stops' => array()
-);
+foreach ($dir as $file) {
+    $tmpFile = $file;
+    $file = 'data/' . $file;
+    if (is_file($file) && substr($tmpFile,0,1)) {
+        $fileLines = file($file);
+        $routeNumber = array_shift($fileLines);
+        list($name, $number) = explode(";", $routeNumber);
 
-$started = false;
-foreach ($file as $line) {
-    $arr = explode(";", $line);
-    $stopName = array_shift($arr);
-    $times = array();
-    foreach ($arr as $s) {
-        $s = str_replace("\n", "", $s);
-        if (preg_match("/[0-9]{1,2}\.[0-9]{1,2}/", $s)) {
-            $started = true;
-            $times[] = $s;
-        }
-    }
-    $db->stops->insert(array('name'=>$stopName));
-    if (count($times) > 0 || $started) {
-        $stop = array(
-            'name' => $stopName,
-            'times' => $times
+        $route = array(
+            'id' => $number,
+            'name' => $name,
+            'stops' => array()
         );
-        $route['stops'][] = $stop;
+
+        $started = false;
+        foreach ($fileLines as $line) {
+            $arr = explode(";", $line);
+            $stopName = array_shift($arr);
+            $times = array();
+            foreach ($arr as $s) {
+                $s = str_replace("\n", "", $s);
+                if (preg_match("/[0-9]{1,2}\.[0-9]{1,2}/", $s)) {
+                    $started = true;
+                    $times[] = $s;
+                }
+            }
+            $db->stops->insert(array('name'=>$stopName));
+            if (count($times) > 0 || $started) {
+                $stop = array(
+                    'name' => $stopName,
+                    'times' => $times
+                );
+                $route['stops'][] = $stop;
+            }
+        }
+        // Flip array and remove all entries of stops that have no times
+        $flipped = array_reverse($route['stops']);
+
+        foreach ($flipped as $key => $tmp) {
+            if (count($tmp['times']) == 0)
+                unset($flipped[$key]);
+            else
+                break;
+        }
+        $route['stops'] = array_reverse($flipped);
+
+        $db->routes->insert($route);
     }
 }
-// Flip array and remove all entries of stops that have no times
-$flipped = array_reverse($route['stops']);
-
-foreach ($flipped as $key => $tmp) {
-    if (count($tmp['times']) == 0)
-        unset($flipped[$key]);
-    else
-        break;
-}
-$route['stops'] = array_reverse($flipped);
-
-$db->routes->insert($route);
