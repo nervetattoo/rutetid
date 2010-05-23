1362436acb2b5658d6a05160bcec8c3476eaadd7
Raymond Julin <raymond.julin@gmail.com>
2010-05-23 17:16:22 +0200

    Moved search.php->index.php

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
-
 require_once("Config.php");
 require_once("View.php");
+require_once("libs/RouteSearch.php");
 
 $db = Config::getDb();
-
 $view = new View;
-$view->display('index.tpl');
+
+if (isset($_GET['from']) && isset($_GET['to'])) {
+    $from = $_GET['from'];
+    $to = $_GET['to'];
+    $time = $_GET['time'];
+
+    $search = new RouteSearch;
+    $hits = $search->search($from, $to, false, false, 15);
+
+    $view->assign('routes', $hits);
+    $view->assign('from', $from);
+    $view->assign('to', $to);
+}
+
+$view->display('search.tpl');
