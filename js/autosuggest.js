dda2dc52ddc59dabc6316330375f3d83ea58a564
Hein Haraldson <hein@raymond.(none)>
2010-05-23 17:19:45 +0200

    Pushing some JS.

--- js/autosuggest.js
+++ js/autosuggest.js
@@ -1,41 +1,56 @@
+/*
+ *  @author     Hein Haraldson Berg
+ *  @email      hein@keyteq.no
+ */
+
 $(function()
 {
     window.setInitialFromValueFromGeolocation = function()
     {
-        $.ajax(
-    	{
-    	    url: 'stops.php',
-    	    data: {
-    	        lat: $('body').data('lat'),
-    	        long: $('body').data('long')
-    	    },
-    	    success: function(data)
-    	    {
-    	        $('#from').val(data.stops[0].name);
-    	    },
-    	    dataType: 'json'
-    	});
+        if($('#from').length && $('#from').val() == '')
+        {
+            $.ajax(
+        	{
+        	    url: 'stops.php',
+        	    data: {
+        	        lat: $('body').data('lat'),
+        	        long: $('body').data('long')
+        	    },
+        	    success: function(data)
+        	    {
+        	        $('#from')
+        	           .val(data.stops[0].name);
+        	    },
+        	    dataType: 'json'
+        	});
+    	}
     }
 
     $('#from, #to')
         .autocomplete(
         {
             source: function(req, add)
-            {				
-				$.ajax(
-				{
-				    url: 'stops.php',
-				    data: {
-				        term: req.term,
-				        lat: $('body').data('lat'),
-				        long: $('body').data('long')
-				    },
-				    success: function(data)
-				    {
-				        fetchStops(data, add);
-				    },
-				    dataType: 'jsonp'
-				});
+            {
+                var from = $('#from').val();
+                
+                if(req.term.length >= 2)
+                {
+    				$.ajax(
+    				{
+    				    url: 'stops.php',
+    				    data: {
+    				        term: req.term,
+    				        from: from,
+    				        lat: $('body').data('lat'),
+    				        long: $('body').data('long')
+    				    },
+    				    success: function(data)
+    				    {
+    				        fetchStops(data, add);
+    				    },
+    				    dataType: 'jsonp'
+    				});
+                }
 			}
         });
         
