855c98f22b18f8af359ab95e83599c848efc39c9
Atmoz <github.com@konto.atmoz.no>
2010-05-23 14:21:35 +0200

    Handle submit

--- insert.php
+++ insert.php
@@ -5,4 +5,13 @@ require_once("View.php");
 
 $db = Config::getDb();
 $view = new View;
+
+if (isset($_POST['stop']) && isset($_POST['time']))
+{
+    $db->times->insert(array(
+        'stop' => $_POST['stop'],
+        'time' => $_POST['time']
+    ));
+}
+
 $view->display('insert.tpl');
